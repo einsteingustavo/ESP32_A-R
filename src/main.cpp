bbd698129cad6245e6fad7422006921a3ef4f0f6
marlonbrado <60429910+marlonbrado@users.noreply.github.com>
2020-07-25 14:33:34 -0300

    Basic cleanup, comment changes, changes to readme.
    Added some details on readme, changed name of project. Changed some comments on main, basic code cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,18 @@
 byte S_ZERO = 34;   //Initial sensor
 byte S_30 = 35;     //30m sensor
 byte S_100 = 25;    //100m sensor
-byte S_C1 = 32;     //1st sensor on the curve
-byte S_C2 = 33;     //2nd sensor on the curve
+byte S_C1 = 32;     //1st sensor on the corner
+byte S_C2 = 33;     //2nd sensor on the corner
 //Debug Pins
-byte LED = 2;       //LED for debug
+byte LED = 2;       //LED for debugging
 
 /*Data Structures*/
 typedef struct
 {
-    volatile unsigned long int time_in_30;
-    volatile unsigned long int time_c_start;
-    volatile unsigned long int time_c_end; 
-    volatile unsigned long int time_in_100;
+    volatile unsigned long time_in_30;
+    volatile unsigned long time_c_start;
+    volatile unsigned long time_c_end; 
+    volatile unsigned long time_in_100;
     
 } packet_ble;       //Package type to send by bluetooth
 
@@ -29,13 +29,13 @@ run_state ss_r = START_;    //Initial secondary/run state setting
 
 packet_ble data;            //Package to send by bluetooth
 bool interrupt = false;     //Autoral support boolean to interrupt functions
-unsigned long int t_curr = 0;   //Current initial time, updated every run
-volatile unsigned long int t_30 = 0, t_c1 = 0, t_c2 = 0, t_100 = 0;     //Time variables
+unsigned long t_curr = 0;   //Current initial time, updated every run
+volatile unsigned long t_30 = 0, t_c1 = 0, t_c2 = 0, t_100 = 0;     //Time variables
        
-/*Functions*/
+/*Function Prototypes*/
 void isr_30m();     //30m interrupt
-void isr_c1();      //Start of curve interrupt
-void isr_c2();      //End of curve interrupt
+void isr_c1();      //Start of corner interrupt
+void isr_c2();      //End of corner interrupt
 void isr_100m();    //100m interrupt
 
 void setup()
@@ -75,8 +75,8 @@ void loop()     //Main loop
                         t_curr = millis();      //Initial time
                     }
                     break;
-                case WAIT_30:   //Wainting for the car to get trough 30m sensor
-                    if (!interrupt && (ss_r == WAIT_30))    //If interrupt isn't active and this is the actual state
+                case WAIT_30:   //Waiting for the car to get trough 30m sensor
+                    if (!interrupt && (ss_r == WAIT_30))    //If interrupt isn't active and this is the current state
                     {
                         interrupt = true;   //Set the support boolean
                         attachInterrupt(digitalPinToInterrupt(S_30), isr_30m, RISING); //Attach interrupt
@@ -90,8 +90,8 @@ void loop()     //Main loop
                         t_100 = millis() - t_curr;
                     }
                     break;
-                case WAIT_C1:   //Wainting for the car to get trough 1st sensor of the curve
-                    if (((millis()- t_curr) - t_30 >  1000) && !interrupt && (ss_r == WAIT_C1)) //If at least one secont has passed and interrupt isn't active and this is the actual state
+                case WAIT_C1:   //Waiting for the car to get trough 1st sensor of the corner
+                    if (((millis()- t_curr) - t_30 >  1000) && !interrupt && (ss_r == WAIT_C1)) //If at least one second has passed and interrupt isn't active and this is the current state
                     {
                         interrupt = true;   //Set the support boolean
                         attachInterrupt(digitalPinToInterrupt(S_C1), isr_c1, RISING);   //Attach interrupt
@@ -104,8 +104,8 @@ void loop()     //Main loop
                         t_100 = millis() - t_curr;
                     }
                     break;
-                case WAIT_C2:   //Wainting for the car to get trough 2nd sensor of the curve
-                    if (((millis()- t_curr) - t_c1 >  1000) && !interrupt && (ss_r == WAIT_C2)) //If at least one secont has passed and interrupt isn't active and this is the actual state
+                case WAIT_C2:   //Waiting for the car to get trough 2nd sensor of the corner
+                    if (((millis()- t_curr) - t_c1 >  1000) && !interrupt && (ss_r == WAIT_C2)) //If at least one second has passed and interrupt isn't active and this is the current state
                     {
                         interrupt = true;   //Set the support boolean
                         attachInterrupt(digitalPinToInterrupt(S_C2), isr_c2, RISING);   //Attach interrupt
@@ -117,8 +117,8 @@ void loop()     //Main loop
                         t_100 = millis() - t_curr;
                     }
                     break;
-                case WAIT_100:  //Wainting for the car to get trough 100m sensor
-                    if (((millis()- t_curr) - t_c2 >  1000) && !interrupt && (ss_r == WAIT_100))    //If at least one secont has passed and interrupt isn't active and this is the actual state
+                case WAIT_100:  //Waiting for the car to get trough 100m sensor
+                    if (((millis()- t_curr) - t_c2 >  1000) && !interrupt && (ss_r == WAIT_100))    //If at least one secont has passed and interrupt isn't active and this is the current state
                     {
                         interrupt = true;   //Set the support boolean
                         attachInterrupt(digitalPinToInterrupt(S_100), isr_100m, RISING);    //Attach interrupt
@@ -147,21 +147,21 @@ void loop()     //Main loop
 
 void isr_30m()  //30m interrupt function
 {
-    ss_r = WAIT_C1;     //Trigger the secondary state for waiting 1st sensor of the curve
+    ss_r = WAIT_C1;     //Trigger the secondary state for waiting 1st sensor of the corner
 
     interrupt = false;  //Reset support boolean
     detachInterrupt(digitalPinToInterrupt(S_30));   //Detach Interrupt
 }
 
-void isr_c1()   //Start of curve interrupt function
+void isr_c1()   //Start of corner interrupt function
 {
-    ss_r = WAIT_C2;     //Trigger the secondary state for waiting 2nd sensor of the curve
+    ss_r = WAIT_C2;     //Trigger the secondary state for waiting 2nd sensor of the corner
 
     interrupt = false;  //Reset support boolean
     detachInterrupt(digitalPinToInterrupt(S_C1));   //Detach Interrupt
 }
 
-void isr_c2()   //End of curve interrupt function
+void isr_c2()   //End of corner interrupt function
 {
     ss_r = WAIT_100;    //Trigger the secondary state for waiting 100m sensor
 
